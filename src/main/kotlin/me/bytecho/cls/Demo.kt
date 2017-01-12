c42e69f8cc9783772c0fae6b3bfd17fa0c2c6792
bytecho <bytecho@163.com>
2017-01-12 20:14:06 +0800

    add Demo.kt to pkg cls

---
+++ src/main/kotlin/me/bytecho/cls/Demo.kt
@@ -0,0 +1,52 @@
+package me.bytecho.cls
+
+/**
+ *
+ * Created by Alan on 2017/1/12.
+ */
+
+fun main(args: Array<String>) {
+    val d = Demo()
+    val d2 = Demo2("demo2")
+
+    val p = Person("NO.9")
+    val p2 = Person2("NO.5", 233233233, 23)
+}
+
+//class with empty body
+class Demo
+
+//class with constructor
+class Demo2(f: String)
+
+//secondary constructor
+class Demo3 {
+    var name:String = "";
+    constructor(name: String) {
+        this.name = name;
+    }
+}
+
+class Demo4(fName: String) {
+    constructor(fName: String, lName: String): this(fName) {
+        println("fName is $fName, lName is $lName")
+    }
+}
+
+class Person constructor(name: String) {
+    init {
+        println("init another person, name is $name")
+    }
+
+    //init properties
+    val prop1 = name.toUpperCase()
+}
+
+class Person2(val name: String, val id: Long, var age: Int) {
+
+    init {
+        println("init Person2: name is $name, id is$id, age is $age")
+    }
+}
+
+
