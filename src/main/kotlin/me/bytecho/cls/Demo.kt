bfde43e2d8405b52a3a9083c6ac793f9a9e069a4
bytecho <bytecho@163.com>
2017-01-13 11:05:04 +0800

    update Demo.kt

--- src/main/kotlin/me/bytecho/cls/Demo.kt
+++ src/main/kotlin/me/bytecho/cls/Demo.kt
@@ -21,14 +21,15 @@ class Demo2(f: String)
 
 //secondary constructor
 class Demo3 {
-    var name:String = "";
+    var name: String = "";
+
     constructor(name: String) {
         this.name = name;
     }
 }
 
 class Demo4(fName: String) {
-    constructor(fName: String, lName: String): this(fName) {
+    constructor(fName: String, lName: String) : this(fName) {
         println("fName is $fName, lName is $lName")
     }
 }
@@ -49,4 +50,34 @@ class Person2(val name: String, val id: Long, var age: Int) {
     }
 }
 
+/*Classes can contain
+--Constructors and initializer blocks
+--Functions
+--Properties
+--Nested and Inner Classes
+--Object Declaration*/
+class MemberDemo {
+    //constructors
+    constructor(f: String)
 
+    //initializer blocks
+    init {
+        println("hello")
+    }
+
+    //func
+    fun prtInfo() {
+        println(toString())
+    }
+
+    //prop
+    public var name: String = ""
+
+    //inner class
+    class Inner
+
+    //obj declaration
+    companion object Instance {
+        fun getInstance(): MemberDemo = MemberDemo("hou")
+    }
+}
