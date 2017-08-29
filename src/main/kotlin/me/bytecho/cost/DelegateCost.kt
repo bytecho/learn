ed9015d9bad178fdba7c8c2df5e63888471fafff
bytecho <bytecho@163.com>
2017-08-29 11:07:49 +0800

    update cost

--- src/main/kotlin/me/bytecho/cost/DelegateCost.kt
+++ src/main/kotlin/me/bytecho/cost/DelegateCost.kt
@@ -1,6 +1,10 @@
 package me.bytecho.cost
 
+import kotlin.properties.Delegates
+
 /**
  *
  * Created by Alan on 2017/8/28.
  */
+val lazyProp:String by lazy { "hello world" }
+val notNullProp:String by Delegates.notNull<String>()
