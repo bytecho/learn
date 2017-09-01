1cd656d1010b0e1ba3576578b244eced58fb9a3e
bytecho <bytecho@163.com>
2017-09-01 15:03:32 +0800

    update cost

--- src/main/kotlin/me/bytecho/cost/Person.kt
+++ src/main/kotlin/me/bytecho/cost/Person.kt
@@ -4,9 +4,19 @@ package me.bytecho.cost
  *
  * Created by Alan on 2017/9/1.
  */
+
+fun main(args: Array<String>) {
+    val p = Person()
+    println(p.isAdult())
+}
 class Person(val name: String = "Alan", var age: Int = 0, val id: Long = -1) {
     init {
         println("$name")
+    }
+
+    fun isAdult():Boolean {
+        println("from declared fun")
+        return age >= 18
     }
 }
 
